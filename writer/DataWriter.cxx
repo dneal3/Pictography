42927372d6678b5e761fe137026b7cab1a3390c9
Daniel Loyd <daniel.loyddan@gmail.com>
2019-02-15 23:12:07 -0800

    push DataWriter with point writing integrated

--- writer/DataWriter.cxx
+++ writer/DataWriter.cxx
@@ -1,44 +1,10 @@
 #include "DataWriter.h"
-#include <vtkPolyData.h>
-#include <vtkPolyDataWriter.h>
-#include <vtkPointData.h>
-#include <vtkPoints.h>
-#include <vtkCellArray.h>
-#include <vtkFloatArray.h>
 using namespace std;
-DataWriter::DataWriter(void)
-{
-}
-void DataWriter::write(vector<double> colors, vector<pair<double, double> > pointsIn, const char *filename)
-{
-	vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
-	int i;
- 	static double x[8][3]={{0,0,0}, {1,0,0}, {1,1,0}, {0,1,0}, {0,0,1}, {1,0,1}, {1,1,1}, {0,1,1}};
- 	static vtkIdType pts[6][4]={{0,1,2}, {4,5,6}, {0,5,4}, {1,6,5}, {3,7,6}, {3,4,7}};
-	vtkPolyData *tri = vtkPolyData::New();
-	vtkPoints *points = vtkPoints::New();
-	vtkCellArray *polys = vtkCellArray::New();
-	vtkFloatArray *scalars = vtkFloatArray::New();
- 	for (i=0; i<8; i++) points->InsertPoint(i,x[i]);
- 	for (i=0; i<6; i++) polys->InsertNextCell(3,pts[i]);
- 	for (i=0; i<8; i++) scalars->InsertTuple1(i,i);
- 	// We now assign the pieces to the vtkPolyData.
- 	tri->SetPoints(points);
- 	points->Delete();
- 	tri->SetPolys(polys);
- 	polys->Delete();
- 	tri->GetPointData()->SetScalars(scalars);
- 	scalars->Delete();
-
-	writer->SetInputData(tri);
-	writer->SetFileName(filename);
-	writer->Write();
-}
 int main(void)
 {
 	DataWriter w;
 	vector<double> c;
-	vector<pair<double, double> >pts;
+	vector<pair<double, double> >pts = {{0,0}, {1,0}, {1,1}, {0,1}, {0,0}, {1,0}, {1,1}, {0,1}, {1,1}};
 	const char * fn = "output";
 	w.write(c, pts, fn);
 }
--- writer/DataWriter.h
+++ writer/DataWriter.h
@@ -2,11 +2,82 @@
 #define DATAWRITER
 #include <iostream>
 #include <vector>
+#include <vtkPolyData.h>
+#include <vtkPolyDataWriter.h>
+#include <vtkPointData.h>
+#include <vtkPoints.h>
+#include <vtkCellArray.h>
+#include <vtkFloatArray.h>
+
+enum ErrorType{NOERR = 0, INVALIDNUMPTS};
+class invalidData : public std::exception
+{
+	ErrorType errorCode;
+	public:
+	invalidData(void){this->errorCode = NOERR;}
+	invalidData SetErr(ErrorType err){errorCode = err; return *this;}
+	std::string errorMsg()
+	{
+		switch(errorCode)
+		{
+			case NOERR:	return "No error detected. Forgot to set errorCode? Contact Daniel for debugging";
+			case INVALIDNUMPTS: return "invalid number of triangles";
+			default:	return "No valid ErrorType detected. Not sure how this happened, contact Daniel for debugging";
+		}
+	}
+} myErr;
 class DataWriter
 {
 	public:
-	DataWriter(void);
+	DataWriter(void){};
 	void write(std::vector<double> colors, std::vector<std::pair<double, double> > pointsIn, const char *filename);
 };
 
+void DataWriter::write(std::vector<double> colors, std::vector<std::pair<double, double> > pointsIn, const char *filename)
+{
+        vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
+        vtkPolyData *tris = vtkPolyData::New();
+        vtkPoints *points = vtkPoints::New();
+        vtkCellArray *polys = vtkCellArray::New();
+        vtkFloatArray *scalars = vtkFloatArray::New();
+        int i, i2=0;
+	try
+	{
+        	const int ptsLen = pointsIn.size();
+		if(ptsLen%3 != 0){throw myErr.SetErr(INVALIDNUMPTS);}//throw invalidData(); } 	//ensure there are enough points to build entirely whole triangles with the data
+		int numTris = ptsLen/3;
+		double ptLocs[3];
+		vtkIdType vertices[3];
+		for(i=0; i<ptsLen; i+=3)
+		{
+			for(i2=0; i2<3; i2++)
+			{
+				vertices[i2] = i+i2;
+				ptLocs[0] = pointsIn[i+i2].first;
+				ptLocs[1] = pointsIn[i+i2].second;
+				ptLocs[2] = 0;
+				points->InsertPoint(i+i2, ptLocs);
+			}
+			polys->InsertNextCell(3, vertices);
+			scalars->InsertTuple1(i,i);
+		}
+        	// We now assign the pieces to the vtkPolyData.
+        	tris->SetPoints(points);
+        	points->Delete();
+        	tris->SetPolys(polys);
+        	polys->Delete();
+        	tris->GetPointData()->SetScalars(scalars);
+        	scalars->Delete();
+	
+	        writer->SetInputData(tris);
+        	writer->SetFileName(filename);
+        	writer->Write();
+	}
+	catch(invalidData e)
+	{
+		cerr << "AN ERROR OCCURED!" << endl;
+		cerr << "**********************************" << endl;
+		cerr << "ERROR: " << e.errorMsg() << endl;
+	}
+}
 #endif
