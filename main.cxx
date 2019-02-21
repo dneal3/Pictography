b19092a4cb5928b3133bc8e28eae42f813ebe114
Daniel Loyd <daniel.loyddan@gmail.com>
2019-02-21 13:29:28 -0800

    remove writer directory and change main to .cxx

--- main.cxx
+++ main.cxx
@@ -4,7 +4,8 @@
 #include <string>
 
 #include "validData.h"
-
+#include "DataWriter.h"
+using namespace std;
 int main()
 {
    // Get the user's message
@@ -27,4 +28,9 @@ int main()
    {
       printf("Data is invalid\n");
    }
+   DataWriter w;
+   vector<double> c = {1.0, 0.3, 0.8, 1.5, 0.7, 1.9, 0.7, 0.12345, 0.663};
+   vector<pair<double, double> >pts = {{0,0}, {1,0}, {1,1}, {0,1}, {0,0}, {1,0}, {1,1}, {0,1}, {1,1}};
+   const char * fn = "output";
+   w.write(c, pts, fn);
 }
