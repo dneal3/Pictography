c9a5a17e0cb11ecc4f3dd33fc270c73d6a16566e
holly <hhardin@cs.uoregon.edu>
2019-03-02 14:56:54 -0800

    Updated files. Encrypt and decrypt take in two arguments. First argument should be filename.vtk and second should be imagename with no .png

---
+++ encrypt.cxx
@@ -0,0 +1,15 @@
+#include "manager.h"
+
+int main(int argc, char* argv[])
+{
+    // Create a manager for the program
+    Manager manager;
+
+    // Encrypt a message
+    manager.Encrypt();
+
+    // Write data to .vtk file
+    manager.WriteData(argv[1], argv[2]);
+
+    return 0;
+}
