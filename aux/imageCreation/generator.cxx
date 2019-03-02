f33a345e279c4d6d67d88c1b269e33e7f0ea6392
hhardin <40405367+hhardin@users.noreply.github.com>
2019-03-02 15:02:22 -0800

    Delete generator.cxx

--- aux/imageCreation/generator.cxx
+++
@@ -1,8 +0,0 @@
-#include "ImageManager.h"
-#include "../io/DataReader.h"
-int main(void)
-{
-	//DataReader::Read("proj1d_geometry.vtk");
-	std::vector<Triangle> temp = DataReader::Read("output");
-	ImageManager::CreateImage(temp, "testOut");
-}
